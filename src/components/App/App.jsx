1964e0f0842afd0014bc74798c38c28e9f33849c
Pedro H G Migacz <pedromigacz@alunos.utfpr.edu.br>
2020-11-21 00:37:31 -0300

    add drop functionality

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,18 +4,32 @@ import styles from './App.module.css'
 
 function App() {
   const [dragOver, setDragOver ] = useState(false)
+  const [file, setFile] = useState([])
+
+  const handleDrop = e => {
+    e.stopPropagation()
+    e.preventDefault()
+    setFile(e.dataTransfer.files[0])
+  }
 
   return (
     <div className={styles.innerDiv}>
       <form>
         <div>
-          <input type="file" id="file" className={styles.fileInput}/>
+          <input
+            onChange={e => setFile(e.target.files[0])}
+            type="file"
+            id="file"
+            className={styles.fileInput}
+          />
           <label
+            onDrop={handleDrop}
+            onDragOver={e => e.preventDefault()}
             onDragEnter={e => setDragOver(!dragOver)}
             onDragLeave={e => setDragOver(!dragOver)}
             className={styles.fileLabel + ' ' + (dragOver && styles.onDragOverClass)}
             htmlFor="file"
-          ><strong>Choose a file</strong> or drop it here &#40;:</label>
+          ><strong>Chose a file</strong> or drop it here &#40;:</label>
           <button type="submit">Convert</button>
         </div>
         <div className={styles.uploadState}>Uploading...</div>
